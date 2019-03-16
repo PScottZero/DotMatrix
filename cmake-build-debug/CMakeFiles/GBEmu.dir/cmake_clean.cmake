a8e398a511d8cbf4f378021fd156f1967dafb299
PScottZero <8pscott@gmail.com>
2019-03-16 17:45:54 -0400

    16 March 2019
    Inital commit

---
+++ cmake-build-debug/CMakeFiles/GBEmu.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GBEmu.dir/gb.cpp.obj"
+  "GBEmu.pdb"
+  "GBEmu.exe"
+  "GBEmu.exe.manifest"
+  "GBEmu.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GBEmu.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
